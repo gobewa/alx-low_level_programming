bead163e7fe620e6c34ce2b54b64ec0e184880ad
gobewa <dawano2020@gmail.com>
2023-09-09 14:46:39 -0700

    101-wildcmp.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+
 int _strlen_recursion(char *s);
 int helper_palindrome(char *s, int len);
 
@@ -14,9 +15,11 @@ int helper_palindrome(char *s, int len);
 int _strlen_recursion(char *s)
 {
 	if (!*s)
-	{
-		return (0);
-	}
+
+	return (0);
+
+	else
+
 	return (_strlen_recursion(s + 1) + 1);
 }
 
